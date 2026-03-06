206e32c07350dd2547a20395d64551fb5cf4a559
Omkar Mane <omkarnamdevmane@gmail.com>
2026-03-06 11:59:44 +0530

    Added python operators.py file

---
+++ operators.py
@@ -0,0 +1,40 @@
+
+# Operators in Python
+
+a = 10
+b = 3
+
+# Arithmetic Operators
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Modulus:", a % b)
+
+# Comparison Operators
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+
+# Logical Operators
+x = True
+y = False
+
+print(x and y)
+print(x or y)
+print(not x)
+
+# Assignment Operators
+num = 5
+num += 3
+print(num)
+
+# Identity Operators
+print(a is b)
+print(a is not b)
+
+# Membership Operators
+name = "Python"
+print("P" in name)
+print("z" not in name)
