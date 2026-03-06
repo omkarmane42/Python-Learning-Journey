9995eaadee9eafee140fb28ad117079550e57f6f
Omkar Mane <omkarnamdevmane@gmail.com>
2026-03-06 12:03:02 +0530

    Added strings.py file

---
+++ strings.py
@@ -0,0 +1,30 @@
+
+# STRING PROGRAMS
+
+text = "Python Programming"
+
+print(text)
+
+# Length
+print(len(text))
+
+# Uppercase
+print(text.upper())
+
+# Lowercase
+print(text.lower())
+
+# Replace
+print(text.replace("Python", "Java"))
+
+# Split
+print(text.split())
+
+# String slicing
+print(text[0:6])
+
+# Reverse string
+print(text[::-1])
+
+# Check substring
+print("Python" in text)
