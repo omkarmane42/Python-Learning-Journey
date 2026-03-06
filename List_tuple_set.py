74284a5336cfeb83781c5e28c019a0ef27de17ca
Omkar Mane <omkarnamdevmane@gmail.com>
2026-03-06 12:03:50 +0530

    Added List_tuple_set.py file

---
+++ List_tuple_set.py
@@ -0,0 +1,28 @@
+# LIST
+
+numbers = [10, 20, 30, 40]
+
+numbers.append(50)
+print(numbers)
+
+numbers.remove(20)
+print(numbers)
+
+print("Length:", len(numbers))
+
+# TUPLE
+
+fruits = ("apple", "banana", "mango")
+print(fruits)
+
+print(fruits[1])
+
+# SET
+
+colors = {"red", "blue", "green"}
+
+colors.add("yellow")
+print(colors)
+
+colors.remove("red")
+print(colors)
