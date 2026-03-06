0c4be07f898713bd9cad4a8e2aef59fb7c6d8f65
Omkar Mane <omkarnamdevmane@gmail.com>
2026-03-06 11:58:28 +0530

    added python_basics.py file

---
+++ python_basics.py
@@ -0,0 +1,40 @@
+
+# Python Basics
+
+# 1. Hello World
+print("Hello Python")
+
+# 2. Variables
+name = "Omkar"
+age = 22
+print(name, age)
+
+# 3. Data Types
+a = 10
+b = 5.5
+c = "Python"
+d = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+# 4. Input from user
+name = input("Enter your name: ")
+print("Welcome", name)
+
+# 5. Type casting
+num = "10"
+num = int(num)
+print(num + 5)
+
+# 6. Multiple variables
+x, y, z = 10, 20, 30
+print(x, y, z)
+
+# 7. Swap two numbers
+a = 5
+b = 10
+a, b = b, a
+print("After swap:", a, b)
